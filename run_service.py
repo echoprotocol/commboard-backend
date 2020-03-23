53fccfca2ee2211b3ddc300117df973c2e3ec1ed
Evgenii Vasilyev <e.vasilev@pixelplex.io>
2020-03-23 09:31:39 +0300

    [ECHOT-330] Update run_service script and compose file

--- run_service.py
+++ run_service.py
@@ -1,7 +1,9 @@
 import os
 import time
 import argparse
+from echopy import Echo
 from utils.db import get_influxdb_client, get_redis_client
+from config import ECHO_URL
 
 
 def ping(client):
@@ -11,25 +13,34 @@ def ping(client):
         return None
 
 
-def run(influxdb, redis, command):
-    if ping(influxdb) and ping(redis):
+def get_head_block_num(echo):
+    try:
+        echo.connect(ECHO_URL)
+        return echo.api.database.get_dynamic_global_properties()["head_block_number"]
+    except Exception:
+        return None
+
+
+def run(echo, influxdb, redis, command):
+    if ping(influxdb) and ping(redis) and get_head_block_num(echo):
         os.system(command)
     else:
         time.sleep(1)
-        run(influxdb, redis, command)
+        run(echo, influxdb, redis, command)
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('service', type=str, choices=['web', 'celery'], help='Select service to run')
-    args = parser.parse_args()
 
-    influxdb_client = get_influxdb_client()
-    redis_client = get_redis_client()
+parser = argparse.ArgumentParser()
+parser.add_argument('service', type=str, choices=['web', 'celery'], help='Select service to run')
+args = parser.parse_args()
 
-    command = 'flask run' if args.service == 'web' else 'celery -A app.celery worker -l debug -B'
+influxdb_client = get_influxdb_client()
+redis_client = get_redis_client()
+echo = Echo()
 
-    run(
-        influxdb_client,
-        redis_client,
-        command
-    )
+command = 'flask run' if args.service == 'web' else 'celery -A app.celery worker -l debug -B'
+run(
+    echo,
+    influxdb_client,
+    redis_client,
+    command
+)
